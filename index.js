ed6c6d4b9ca17af17a053bc0c5c044c1c8794999
Rodrigo Oliveira Santos <rodrigoos@dcomp.ufs.br>
2023-08-23 12:33:55 -0300

    elegant label for inputs with jquery

--- index.js
+++ index.js
@@ -9,3 +9,41 @@ if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localS
 } else {
     document.documentElement.classList.remove('dark')
 }
+
+// jquery para label dos inputs
+$(document).ready(function () {
+    const inputs = document.querySelectorAll(".primary-input");
+
+    const handleFocus = ({
+        target
+    }) => {
+        const label = target.previousElementSibling;
+        label.classList.remove("text-sm");
+        label.classList.remove("top-2/4");
+        label.classList.remove("left-3");
+        label.classList.add("text-xs");
+        label.classList.add("top-0");
+        label.classList.add("left-6");
+        label.classList.add("border-[1.5px]");
+        label.classList.add("border-[#08246c]");
+    };
+
+    const handleFocusOut = ({
+        target
+    }) => {
+        if (target.value === "") {
+            const label = target.previousElementSibling;
+            label.classList.remove("text-xs");
+            label.classList.remove("top-0");
+            label.classList.remove("left-6");
+            label.classList.remove("border-[1.5px]");
+            label.classList.remove("border-[#08246c]");
+            label.classList.add("text-sm");
+            label.classList.add("top-2/4");
+            label.classList.add("left-3");
+        }
+    };
+
+    inputs.forEach((input) => input.addEventListener("focus", handleFocus));
+    inputs.forEach((input) => input.addEventListener("focusout", handleFocusOut));
+});
